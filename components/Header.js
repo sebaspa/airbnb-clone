a180841ccd6b3049881e952bdf8aaaad8b205e55
sebaspa <sebaspa0107@gmail.com>
2021-08-11 11:28:20 -0500

    Crea el menu del lado derecho del banner

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
 
 function Header() {
   return (
@@ -13,8 +19,20 @@ function Header() {
         />
       </div>
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-        <input type="text" className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400" placeholder="Start your search" />
+        <input
+          type="text"
+          className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400"
+          placeholder="Start your search"
+        />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
+      <div className="flex space-x-4 items-center justify-end text-gray-500">
+        <p className="hidden md:inline">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon class="h-6" />
+        </div>
       </div>
     </header>
   );
