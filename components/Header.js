4caf693ea3aa3f3ad2e6edbe1bfa54fc0ec3d8e2
sebaspa <sebaspa0107@gmail.com>
2021-08-11 10:51:44 -0500

    Crear barra de búsqueda en el header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,22 @@
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
+
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input type="text" className="pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400" placeholder="Start your search" />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
+    </header>
   );
 }
 
